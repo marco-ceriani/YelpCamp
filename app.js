88da566374b76a813a776da8820fd41892bdce0c
Marco Ceriani <shikamaru84@gmail.com>
2018-11-02 11:02:24 +0100

    Added SHOW route

--- app.js
+++ app.js
@@ -13,7 +13,8 @@ mongoose.connect('mongodb://localhost/yelp_camp', { useNewUrlParser: true })
 // SCHEMA SETUP
 var campgroundSchema = new mongoose.Schema({
 	name: String,
-	image: String
+	image: String,
+	description: String
 })
 var Campground = mongoose.model('Campground', campgroundSchema)
 
@@ -21,20 +22,23 @@ app.get('/', function(req, res){
 	res.render('landing')
 })
 
+// INDEX
 app.get('/campgrounds', function(req, res) {
 	Campground.find({}, function(err, allCampgrounds) {
 		if (err) {
 			console.log(err)
 		} else {
-			res.render('campgrounds', {campgrounds : allCampgrounds})
+			res.render('index', {campgrounds : allCampgrounds})
 		}
 	})
 })
 
+// CREATE
 app.post('/campgrounds', function(req, res) {
 	var newCampground = {
 		name: req.body.name,
-		image: req.body.image
+		image: req.body.image,
+		description: req.body.description
 	}
 	Campground.create(newCampground, function(err, camp) {
 		if (err) {
@@ -43,13 +47,25 @@ app.post('/campgrounds', function(req, res) {
 			res.redirect('/campgrounds')
 		}
 	})
-	
 })
 
+// NEW
 app.get('/campgrounds/new', function(req, res) {
 	res.render('new.ejs')
 })
 
+// SHOW
+app.get('/campgrounds/:id', function(req, res) {
+	let id = req.params.id
+	Campground.findById(id, function(err, camp) {
+		if (err) {
+			console.log(err)
+		} else {
+			res.render('show', {campground: camp})
+		}
+	})
+	
+})
 
 app.listen('3000', 'localhost', function() {
 	console.log('YelpCamp started at port 3000')
