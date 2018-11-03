c655a6750b54b08fa78c556e965b9f7755065861
Marco Ceriani <shikamaru84@gmail.com>
2018-11-03 14:09:56 +0100

    Added new comment feature

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var express    = require('express'),
 	bodyParser = require('body-parser'),
 	mongoose   = require('mongoose'),
 	Campground = require('./models/campground')
+	Comment    = require('./models/comment')
 	seedDB     = require('./seeds')
 
 app.set('view engine', 'ejs')
@@ -25,7 +26,7 @@ app.get('/campgrounds', function(req, res) {
 		if (err) {
 			console.log(err)
 		} else {
-			res.render('index', {campgrounds : allCampgrounds})
+			res.render('campgrounds/index', {campgrounds : allCampgrounds})
 		}
 	})
 })
@@ -48,7 +49,7 @@ app.post('/campgrounds', function(req, res) {
 
 // NEW
 app.get('/campgrounds/new', function(req, res) {
-	res.render('new.ejs')
+	res.render('campgrounds/new')
 })
 
 // SHOW
@@ -61,9 +62,44 @@ app.get('/campgrounds/:id', function(req, res) {
 			if (err) {
 				console.log(err)
 			} else {
-				res.render('show', {campground: camp})
+				res.render('campgrounds/show', {campground: camp})
 			}
 		})
+})
+
+// ================================
+// COMMENTS ROUTES
+// ================================
+
+// NEW
+app.get('/campgrounds/:id/comments/new', function(req, res) {
+	Campground.findById(req.params.id, function(err, campground) {
+		if (err) {
+			console.log(err)
+		} else {
+			res.render('comments/new', {campground: campground})
+		}
+	})	
+})
+
+// CREATE
+app.post('/campgrounds/:id/comments', function(req, res) {
+	Campground.findById(req.params.id, function(err, camp) {
+		if (err) {
+			console.log(err)
+			res.redirect('/campgrounds')
+		} else {
+			Comment.create(req.body.comment, function(err, comment) {
+				if (err) {
+					console.log(err)
+				} else {
+					camp.comments.push(comment)
+					camp.save()
+					res.redirect('/campgrounds/'+ camp._id)
+				}
+			})
+		}
+	})
 })
 
 app.listen('3000', 'localhost', function() {
