753e4cfefa492a7a6387e424e101d80e0844bed4
Marco Ceriani <shikamaru84@gmail.com>
2018-11-07 22:21:21 +0100

    Added Update and Destroy on comments

--- routes/comments.js
+++ routes/comments.js
@@ -41,6 +41,43 @@ router.post('/', isLoggedIn, function(req, res) {
     })
 })
 
+// Comments Edit
+router.get('/:comment_id/edit', function(req, res) {
+    // from parent route
+    campgroundId = req.params.id
+    Comment.findById(req.params.comment_id, function(err, comment) {
+        if (err) {
+            res.redirect('back')
+        } else {
+            res.render('comments/edit', 
+                {campground_id: campgroundId, comment:comment})
+        }
+    })
+})
+
+// Comments Update
+router.put('/:comment_id', function(req, res) {
+    Comment.findByIdAndUpdate( req.params.comment_id, req.body.comment, function(err, camp) {
+        if (err) {
+            res.redirect('back')
+        } else {
+            res.redirect('/campgrounds/' + req.params.id)
+        }
+    })
+})
+
+// Comments Destroy
+router.delete('/:comment_id', function(req, res) {
+    Comment.findByIdAndRemove(req.params.comment_id, function(err) {
+        if (err) {
+            console.log(err)
+            res.redirect('/campgrounds/' + req.params.id)
+        } else {
+            res.redirect('/campgrounds/' + req.params.id)
+        }
+    })
+})
+
 function isLoggedIn(req, res, next) {
     if (req.isAuthenticated()) {
         return next()
