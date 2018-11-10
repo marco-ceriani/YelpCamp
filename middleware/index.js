0469101dd4811fb4350384aced0bd3745699a246
Marco Ceriani <marco-ceriani@users.noreply.github.com>
2018-11-10 11:05:34 +0100

    Added flash messages

--- middleware/index.js
+++ middleware/index.js
@@ -8,6 +8,7 @@ middlewareObj.isLoggedIn = function(req, res, next) {
     if (req.isAuthenticated()) {
         return next()
     } else {
+        req.flash("error", "You need to be logged in")
         res.redirect('/login')
     }
 }
@@ -16,16 +17,22 @@ middlewareObj.checkCommentAuthor = function(req, res, next) {
     if (req.isAuthenticated()) {
         Comment.findById(req.params.comment_id, function(err, foundComment) {
             if (err) {
+                req.flash("error", "Comment not found")
+                res.redirect('back')
+            } else if (!foundComment) {
+                req.flash("error", "Comment not found")
                 res.redirect('back')
             } else {
                 if (foundComment.author.id.equals(req.user._id)) {
                     return next()
                 } else {
+                    req.flash("error", "You don't have the required permission")
                     res.redirect('back')
                 }
             }
         })
     } else {
+        req.flash("error", "You need to be logged in to do that")
         res.redirect('back')
     }
 }
@@ -34,17 +41,23 @@ middlewareObj.checkCampgroundAuthor = function(req, res, next) {
     if (req.isAuthenticated()) {
         Campground.findById(req.params.id, function(err, campground) {
             if (err) {
+                req.flash("error", "Campground not found")
+                res.redirect('back')
+            } else if (!campground) {
+                req.flash("error", "Campground not found")
                 res.redirect('back')
             } else {
                 // author.id is an object not a string
                 if (campground.author.id.equals(req.user._id)) {
                     return next()
                 } else {
+                    req.flash("error", "You don't have the required permission")
                     res.redirect('back')
                 }
             }
         })
     } else {
+        req.flash("error", "You need to be logged in to do that")
         res.redirect('back')
     }   
 }
--- routes/index.js
+++ routes/index.js
@@ -21,9 +21,10 @@ router.post('/register', function(req, res){
     User.register(newUser, req.body.password, function(err, user) {
         if (err) {
             console.log('error creating user ' + username + ' :' + err)
-            return res.render('register')
+            return res.render('register', {"error": err.message})
         }
-        passport.authenticate('local')(req, res, function(){
+        passport.authenticate('local')(req, res, function() {
+            req.flash("success", "Welcome to YelpCamp " + user.username)
             res.redirect('campgrounds')
         })
     })
@@ -45,16 +46,8 @@ router.post('/login',
 // Log out
 router.get('/logout', function(req, res) {
     req.logout()
+    req.flash("success", "Logged out!")
     res.redirect('/campgrounds')
 }) 
-
-
-function isLoggedIn(req, res, next) {
-    if (req.isAuthenticated()) {
-        return next()
-    } else {
-        res.redirect('/login')
-    }
-}
 
 module.exports = router
