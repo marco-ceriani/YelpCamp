7eba3b7d6bf98da236948733a69c73c7975fdead
Marco Ceriani <shikamaru84@gmail.com>
2018-11-22 16:29:06 +0100

    Added Admin user

--- models/user.js
+++ models/user.js
@@ -3,8 +3,13 @@ var passportLocalMongoose = require('passport-local-mongoose')
 
 var UserSchema = new mongoose.Schema({
     username: String,
-    password: String
+    password: String,
+    role: { type: String, default: 'USER' }
 })
 UserSchema.plugin(passportLocalMongoose)
+User = mongoose.model('User', UserSchema)
 
-module.exports = mongoose.model('User', UserSchema)
+User.register({username: 'admin', role: 'ADMIN'}, 'istrator', function(err, user) {
+})
+
+module.exports = User
