175f9d1185723a0b29cd24c9c789c81db840689d
Marco Ceriani <marco-ceriani@users.noreply.github.com>
2018-11-25 16:21:11 +0100

    Rewritten seeds file

--- seeds.js
+++ seeds.js
@@ -3,109 +3,101 @@ var Campground = require("./models/campground");
 var Comment    = require("./models/comment");
 var User       = require("./models/user");
 
-var users = [
-    {name: "pippo", password: "pippo"},
-    {name: "marco", password: "polo"},
-    {name: "mickey", password: "mouse"}
+var userSeeds = [
+    {username: "admin", password: "istrator", role: "ADMIN"},
+    {username: "marco", password: "polo", fullName: "Marco Polo", avatar : "https://aff.bstatic.com/images/hotel/max500/850/85071113.jpg"},
+    {username: "pippo", password: "pippo"},
+    {username: "mickey", password: "mouse", fullName: "Mickey Mouse"}
 ]
 
-var data = [
+var campSeeds = [
     {
         name: "Cloud's Rest", 
-        image: "https://farm4.staticflickr.com/3795/10131087094_c1c0a1c859.jpg",
-        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
+        image: "/photos/13208593975.png",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+        price: '9.99'
     },
     {
         name: "Desert Mesa", 
-        image: "https://farm6.staticflickr.com/5487/11519019346_f66401b6c1.jpg",
-        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
+        image: "/photos/18103628621.png",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+        price: '5.40'
     },
     {
         name: "Canyon Floor", 
-        image: "https://farm1.staticflickr.com/189/493046463_841a18169e.jpg",
-        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
+        image: "/photos/3820664827.png",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+        price: '7.50'
     },
     {
         name: "Salmon Creek", 
         image: "/photos/1208201.png",
-        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+        price: '12.00'
     },
     {
         name: "Sunny Side", 
         image: "/photos/1851092.png",
-        description: "A sunny campground with a breath breaking landscape"
+        description: "A sunny campground with a breath breaking landscape",
+        price: '1.99'
     },
     {
         name: "Hell's Camp", 
         image: "/photos/1867275.png",
-        description: "It's great to tell ghost's stories in this place frozen as hell"
+        description: "It's great to tell ghost's stories in this place frozen as hell",
+        price: '6.66'
     }
 ]
 
-function seedUsers(next) {
-    User.remove({}, function(err) {
-        if (err) {
-            console.log('error removing users: ' + err)
-        } else {
-            console.log('removed all users')
-            count = 0
-            users.forEach(function(user) {
-                var newUser = new User({username: user.name})
-                User.register(newUser, user.password, function(err, createdUser) {
-                    if (err) {
-                        console.log('error creating user ' + user.name + ' : ' + err)
-                    } else {
-                        console.log('created user ' + createdUser.username)
-                        user.id = createdUser._id
-                        count++
-                        if (count == users.length) {
-                            next()
-                        }
-                    }
-                })
-            })
-        }
-    })
-}
+var commentSeeds = [
+    'My hands are writing words.',
+    'this is a random comment!',
+    'blah blah blah',
+    'The best in the world!',
+    'Great!',
+    'First!',
+    'OMG! I got top comment!'
+]
 
-function seedDB() {
-    seedUsers(function() {
-        console.log(users)
-    })
-/*
-    // Remove all campgrounds
-    Campground.remove({}, function(err) {
-        if (err) {
-            console.log(err)
-        } else {
-            console.log('Removed all campgrounds')
-            // Seed the campgrounds
-            data.forEach(function(camp) {
-                Campground.create(camp, function(err, campground) {
-                    if (err) {
-                        console.log(err)
-                    } else {
-                        console.log('Added campground ' + campground.name)
-                        // create a comment
-                        Comment.create({
-                            text: 'This place is great, but I wish there was internet',
-                            author: 'Homer'
-                        }, 
-                        function(err, comment){
-                            if (err) {
-                                console.log(err)
-                            } else {
-                                campground.comments.push(comment)
-                                campground.save()
-                                console.log('Added comment to ' + campground.name)
-                            }
-                        })
-                    }
-                })
-            })
+async function seedDB() {
+
+    await User.deleteMany({})
+    let users = await Promise.all(userSeeds.map(async user => {
+        let newUser = new User(user)
+        return User.register(newUser, user.password)
+    }))
+    users.shift()
+    function randomUser() {
+        return users[Math.floor(Math.random() * users.length)]
+    }
+    
+    await Campground.deleteMany({})
+    await Comment.deleteMany({})
+
+    for (const seed of campSeeds) {
+        let author = randomUser()
+        const newCampgroundData = {
+            ...seed,
+            author : { 
+                id: author._id,
+                username: author.username
+            }
         }
-    })
-    */
+        let campground = await Campground.create(newCampgroundData)
+        for (let i = 0; i < 2; i++) {
+            let author = randomUser()
+            let commentInfo = {
+                text: commentSeeds[Math.floor(Math.random() * commentSeeds.length)],
+                author : { 
+                    id: author._id,
+                    username: author.username
+                }
+            }
+            let comment = await Comment.create(commentInfo)
+            campground.comments.push(comment)
+            await campground.save()
+        }
+    }
 
 }
 
